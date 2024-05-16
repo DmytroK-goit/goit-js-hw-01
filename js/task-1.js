4ce5ad8811752147b6db8fa9900581b2427149c4
Dmytro Kovbasiuk <k0vbasyuk.dim0n@gmail.com>
2024-05-16 13:29:29 +0300

    repair

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,9 @@
 
 function makeTransaction(quality, pricePerDroid) { 
     let totalPrice = quality * pricePerDroid
-        console.log('"You ordered ' + quality + ' droids worth ' + totalPrice + ' credits!"');
+    return "You ordered " + quality + " droids worth " + totalPrice + " credits!"
+       
 }
-makeTransaction(5, 3000)
-makeTransaction(3, 1000)
-makeTransaction(10, 500)
+console.log(makeTransaction(5, 3000))
+console.log(makeTransaction(3, 1000))
+console.log(makeTransaction(10, 500))
--- js/task-3.js
+++ js/task-3.js
@@ -2,10 +2,10 @@ function getElementWidth(content, padding, border) {
     let contentWidth = parseFloat(content)
     let paddingWidth = parseFloat(padding)
     let borderWidth = parseFloat(border)
-    console.log(contentWidth + paddingWidth*2+ borderWidth*2);
+    return contentWidth + paddingWidth*2+ borderWidth*2
    
 }
 
-getElementWidth("50px", "8px", "4px")
-getElementWidth("60px", "12px", "8.5px")
-getElementWidth("200px", "0px", "0px")
+console.log(getElementWidth("50px", "8px", "4px"))
+console.log(getElementWidth("60px", "12px", "8.5px"))
+console.log(getElementWidth("200px", "0px", "0px"))
